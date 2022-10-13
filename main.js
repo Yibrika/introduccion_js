fb60557af2dc94805ad0c1892d8c0430034828a4
yibrika <yibrika@outlook.com>
2022-10-12 19:40:14 -0500

    js

---
+++ main.js
@@ -0,0 +1,23 @@
+var yibrikacarreño = [
+    personalInformation ={
+        nombre   : "yibrika",
+        apellido : "carreño",
+        edad     : 32,
+        developer: true,
+    },
+    birthDay            = {
+        day      : 22,
+        month    : "abril",
+        year     : 1989
+    },
+    favoriteBook        = {
+        title    : "pideme lo que quieras",
+        autor    : "megan maxwell",
+        date     : 2012,
+        url      : "https://www.planetadelibros.com.ar/libros_contenido_extra/36/35397_PidemeLoQueQuieras_PrimerCap.pdf",
+    },
+    hobby               = ["playa", "musica"],
+    function myFavouriteActivity(){
+        return "disfrutar de la playa"
+    }
+]
